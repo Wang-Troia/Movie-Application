3eeebbbd8a9704c73f5a346784de31351aecceb2
liliwang <monsi0510@hotmail.com>
2020-09-23 11:54:14 -0500

    Add some features

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 let loader = document.querySelector('.loader');
 let content = document.querySelector('.content');
-window.addEventListener('load', () => {
-    loader.style.display = 'none';
-    content.classList.remove('invisible');
-});
+let renderdiv = document.getElementById('renderMovie');
+// window.addEventListener('load', () => {
+//     loader.style.display = 'none';
+//     content.classList.remove('invisible');
+// });
 
-const movieUrl = 'https://synonymous-evening-millennium.glitch.me/movies'
+const movieUrl = 'https://synonymous-evening-millennium.glitch.me/movies';
 
 const getMovies = () => fetch(movieUrl)
     .then(res => res.json())
-    .then(res => console.log(res))
-    .catch(console.error);
+    .catch(err => console.error(err));
 
-getMovies()
+getMovies().then(movies => {
+    console.log(movies);
+    movies.forEach(movie => renderMovie(movie));
+    loader.style.display = 'none';
+    content.classList.remove('invisible');
+})
+
+function renderMovie(movie) {
+    let output = `<div>Movie Title: ${movie.title}</div>
+                  <div>Rating: ${movie.rating}</div>`;
+    renderdiv.innerHTML += output;
+}
+
+
+
+var stars = document.getElementsByClassName('star');
+for (let i = 0; i < stars.length; i++) {
+    let id = stars[i].getAttribute('data-value');
+    stars[i].addEventListener('mouseenter', () => document.getElementById('review-result').innerText = id);
+}
